c7991ea427d2bc1cb40829eee5e0ca864cea4b74
Alena Shikhanova <shikhanova@arview.pro>
2023-08-09 11:48:35 +0300

    add angular material

--- frontend/apps/llp/src/app/app.module.ts
+++ frontend/apps/llp/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
